b35b056084be45624844f265d4c171dcd516ffa6
Nasim <nasimhelal123>
2023-01-02 19:36:46 +0600

    Navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,34 @@
+import { Link } from "react-router-dom";
+import { BsCart3 } from "react-icons/bs";
+
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <div className="navbar-bg bg-violet-500 text-violet-50">
+      <div className="navbar container mx-auto flex justify-between items-center h-20">
+        <div className="left">
+          <span className="logo text-xl font-semibold">
+            tech<span className="text-orange-500">Alpha</span>
+          </span>
+        </div>
+        <div className="right flex gap-5 items-center ">
+          <Link to="/" className="nav-link duration-300">
+            Home
+          </Link>
+          <Link to="products" className="nav-link  duration-300">
+            Products
+          </Link>
+          <Link to="cart">
+            <span className="cart-icon flex items-center relative">
+              <BsCart3 />
+              <span className="cart-counter absolute -top-3 -right-3 text-xs flex items-center justify-center bg-orange-500 w-5 h-5 rounded-full font-medium">
+                10
+              </span>
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
