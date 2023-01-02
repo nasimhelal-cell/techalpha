c9762d2da4863dc1066ba2a2570812a240e8e0f3
Nasim <nasimhelal123>
2023-01-02 18:15:04 +0600

    Router set up done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
-  return <div className="flex justify-center">nasm</div>;
+  return (
+    <>
+      <div className="app min-h-screen bg-gray-50 text-gray-700">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>{" "}
+      <Footer />
+    </>
+  );
 };
 
 export default App;
