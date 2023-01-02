9adefb23665ab72826a802f6e8b3a41f978e4386
Nasim <nasimhelal123>
2023-01-02 21:03:43 +0600

    slider added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,11 @@
+import Slider from "../components/Slider";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home overflow-x-hidden">
+      <Slider />
+    </div>
+  );
 };
 
 export default Home;
